627fe784316e87e45e2009cbc92a2f958f10d1f6
liqt91 <liqt91@qq.com>
2017-10-30 18:12:53 +0800

    demo

--- config.py
+++ config.py
@@ -1,1 +1,3 @@
 REDIS_URL = "redis://:@localhost:6379"
+
+JSON_AS_ASCII = False
--- ocr-text-edit.py
+++ ocr-text-edit.py
@@ -3,6 +3,9 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+from flask import jsonify
+from flask import redirect
+from flask import url_for
 from flask.ext.redis import FlaskRedis
 import json
 
@@ -21,21 +24,44 @@ def hello_world():
     #读取redis方法
     result = redis_store.hgetall('OCR.INVOICE.20171010.JPG')
     result = json.dumps(result ,ensure_ascii=False)
-    return render_template("items_table.html", result = result)
+    # return render_template("items_table.html", result = result)
+    return redirect(url_for('getItems',name='OCR.INVOICE.20171010.JPG'))
 
-@app.route('/items')
-@app.route('/items/<string:name>')
+@app.route('/items',methods=['GET','POST'])
+@app.route('/items/<string:name>',methods=['GET','POST'])
 def getItems(name=None):
     if not name:
         items = redis_store.keys()
+        keyWord = request.values.get('keyWord') or ''
+        print keyWord
+        items = [x for x in items if x.startswith('OCR.') and keyWord.lower() in x.lower()]
         print items
-        items = [x for x in items if x.startswith('OCR.')]
-        items = [x for x in items if x.startswith('OCR.')]
         return render_template("items_table.html", items=items)
     else:
-        print name
-        kvs = redis_store.hgetall(name)
-        # kvs = json.dumps(kvs).decode("unicode-escape")
-        return render_template("item_detail.html", kvs=kvs, item_name=name)
+        if request.method == 'GET':
+            kvs = redis_store.hgetall(name)
+            # kvs = json.dumps(kvs).decode("unicode-escape")
+            return render_template("item_detail.html", kvs=kvs, item_name=name)
+        elif request.method == 'POST':
+            type = request.values.get('type')
+            key = request.values.get('key')
+            value = request.values.get('value')
+            if request.values.get('delete'):
+                key = request.values.get('key')
+                redis_store.hdel(name, key)
+                return jsonify(msg='删除成功', code=200)
+            print type, key, value
+            if redis_store.hexists(name, key):
+                print name, key, value
+                redis_store.hset(name, key, value)
+            else:
+                if not request.values.get('origin'):
+                    return jsonify(msg='找不到原值', code=400)
+                with redis_store.pipeline() as pipe:
+                    pipe.multi()
+                    redis_store.hdel(name, request.values.get('origin'))
+                    redis_store.hset(name, key, value)
+                    pipe.execute()
+            return jsonify(msg='修改成功',code=200)
 if __name__ == '__main__':
     app.run()
